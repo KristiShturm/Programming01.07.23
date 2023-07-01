3f8e18a16ef28ac9dc30c99e6ca51504eeeaa1eb
Kristina <misskristsi@yandex.ru>
2023-07-01 08:16:19 +0600

    Project002

---
+++ Seminars/Seminar01/Project002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Напишите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Напишите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) 
+{
+       Console.WriteLine($"Больше число {a}");
+}
+else 
+{
+       Console.WriteLine($"Больше число {b}");
+}
