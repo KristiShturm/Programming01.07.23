511e19f504fb0d6eea3fc78cd08c29b0fe1e7a88
Kristina <misskristsi@yandex.ru>
2023-07-02 06:18:13 +0600

    Project003

---
+++ Seminars/Seminar01/Project003/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Напишите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Напишите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Напишите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write($"Наибольшее число {max} ");
+
+
