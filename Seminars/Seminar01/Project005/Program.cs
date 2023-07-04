1d88abc677524e181a517edab6d8b652ed01c3cf
Kristina <misskristsi@yandex.ru>
2023-07-04 08:33:24 +0600

    Project005

---
+++ Seminars/Seminar01/Project005/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Напишите первое число: ");
+int first = int.Parse(Console.ReadLine());
+
+Console.Write("Напишите второе число: ");
+int second = int.Parse(Console.ReadLine());
+
+if(first > second)
+{
+    second *= second;
+}
+else
+{
+    first *= first;
+}
+if(first == second)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
