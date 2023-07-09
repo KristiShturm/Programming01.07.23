35fce9e7bc9732960425734fb4563e1b54bac910
Kristina <misskristsi@yandex.ru>
2023-07-09 08:37:17 +0600

    Palindrom

---
+++ Seminars/Seminar04/Palindrom/Program.cs
@@ -0,0 +1,30 @@
+﻿//Проверить число, является ли палиндромом
+
+bool IsPalindrome(int number)
+{
+    int revNumber = 0;
+    int sourceNumber = number;
+
+    while (sourceNumber != 0)
+    {
+        int pop = sourceNumber % 10;
+        sourceNumber = sourceNumber / 10;
+
+        //Формирование нового числа (обратное от исходного)
+        revNumber = (revNumber * 10) + pop;
+    }
+
+    bool isResult = revNumber == number;
+
+    return isResult;
+}
+
+//bool result = IsPalindrome(number);
+//Console.WriteLine(result);
+
+Console.Write("Напишите число: ");
+int number = int.Parse(Console.ReadLine());
+
+bool result = IsPalindrome(number);
+Console.WriteLine($"Число является палиндромом: {result}");
+
