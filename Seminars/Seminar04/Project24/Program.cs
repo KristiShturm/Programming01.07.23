6bc2e4a8bafe44d3391d8f7c6efe649e6aab38dd
Kristina <misskristsi@yandex.ru>
2023-07-09 11:24:14 +0600

    project24

---
+++ Seminars/Seminar04/Project24/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+//Метод суммы в диапозоне чисел
+int GetSumRange(int startNumber, int endNumber)
+{
+    int sum = 0;
+    for (int i = startNumber; i <= endNumber; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+//Запрос исходного числа от пользователя
+Console.Write("Напишите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int sum = GetSumRange(1,a);
+Console.WriteLine($"Сумма чисел: {sum}");
