56a8b367688dbc33b599360b4012c77d268da071
Kristina <misskristsi@yandex.ru>
2023-07-10 05:42:08 +0600

    project28

---
+++ Seminars/Seminar04/Project28/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120
+*/
+
+int GetComposition (int number)
+{
+    int composition = 1;
+    for(int i = 1; i <= number; i++)
+    {
+        composition = composition * i;     
+    }
+    return composition;
+}
+
+Console.Write("Напишите число: ");
+int num = int.Parse(Console.ReadLine());
+
+num = GetComposition (num);
+Console.Write($"Произвеение чисел: {num}");
