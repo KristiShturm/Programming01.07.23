ecc44c1203b213aa141b55f4aad2fef10597a37b
Kristina <misskristsi@yandex.ru>
2023-07-09 11:38:40 +0600

    project26

---
+++ Seminars/Seminar04/Project26/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+int GetCountDigital(int number)
+{
+    int count = 0;
+    int number1 = number;
+    while(number1 != 0)
+    {
+        number1 = number1 / 10;
+        count ++;
+    }
+    return count;
+}
+
+Console.Write("Напишите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int res = GetCountDigital(a);
+Console.Write($"Количество цифр: {res}");
