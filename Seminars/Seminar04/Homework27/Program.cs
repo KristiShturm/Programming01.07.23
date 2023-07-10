c89e045fdd6ed6a53556da1dc03bb814db436ddc
Kristina <misskristsi@yandex.ru>
2023-07-10 11:56:56 +0600

    Homework27

---
+++ Seminars/Seminar04/Homework27/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе*/
+
+int GetSum (int number)
+{
+    int number2 = number;
+    int result = 0;
+    while(number2 != 0)
+    {
+        result += number2 % 10;
+        number2 = number2 /10;
+    }
+    return result;
+}
+
+Console.Write("Напишите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int res = GetSum(a);
+Console.WriteLine($"Сумма цифр в числе: {res}");
+
