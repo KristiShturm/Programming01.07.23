365365ab381771e0d5d75d2e4d5b247d464b154c
Kristina <misskristsi@yandex.ru>
2023-07-10 11:33:00 +0600

    Homework04

---
+++ Seminars/Seminar04/Homework04/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите метод, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+*/
+
+double GetPow(double numberA, double numberB)
+{
+    double result = 0;
+    result = Math.Pow(numberA , numberB);
+    return result;
+}
+
+
+Console.Write("Напишите первое число: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Напишите второе число: ");
+double b = double.Parse(Console.ReadLine());
+
+double result = GetPow(a , b);
+Console.Write($"{result}");
